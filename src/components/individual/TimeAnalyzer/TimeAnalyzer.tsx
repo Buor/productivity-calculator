d2dafd75084e02ca2c0e53b430dc1e9e1086ade9
Buor <buor0@mail.ru>
2022-01-23 11:05:36 +0300

    feat: TimeAnalyzer component styled and structured

--- src/components/individual/TimeAnalyzer/TimeAnalyzer.tsx
+++ src/components/individual/TimeAnalyzer/TimeAnalyzer.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {Box} from "@chakra-ui/react"
+import {Box, Button, Text} from '@chakra-ui/react'
+import {NumeratedTextarea} from '../../ui/NumeratedTextarea';
 
 interface IProps {
     
@@ -7,8 +8,12 @@ interface IProps {
 
 export const TimeAnalyzer: React.FC<IProps> = () => {
     return (
-        <Box>
-            This is the time analyzer page!
+        <Box d={'flex'} flexDirection={'column'}>
+            <Text textAlign={'center'} as={'h2'} fontSize={36}>Enter your actions!</Text>
+            <Box mt={4} d={'flex'} justifyContent={'center'}>
+                <NumeratedTextarea placeholder={'Enter your actions here'}/>
+            </Box>
+            <Button mt={8} alignSelf={'center'} colorScheme={'yellow'}>Analyze</Button>
         </Box>
     )
 }
