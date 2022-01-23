a91c82f8b6a412ecb9c37157630846239a3972db
Buor <buor0@mail.ru>
2022-01-23 10:57:05 +0300

    feat: replaced main font with Segou UI

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,10 +1,18 @@
-import {extendTheme} from "@chakra-ui/react"
+import {extendTheme} from '@chakra-ui/react'
 
 export const customTheme = extendTheme({
     colors: {
         custom: {
             dark: 'rgb(18,18,20)',
             medium: 'rgb(37,36,39)'
+        }
+    },
+    styles: {
+        global: {
+            body: {
+                'fontFamily': 'Segoe UI, sans-serif',
+                'fontStyle': 'normal'
+            }
         }
     }
 })
