e6b1fe97d366a1514ed2e5d4bff67f8524fc4081
Buor <buor0@mail.ru>
2022-01-28 16:22:10 +0300

    feat: action type and description added, some fixes
    - Now every action has it's own type. That will
    help to build the advice system and calculate
    productivity based on some specific actions
    like sport and etc. Type is optional and
    'default' by default.
    - Now every action has description. It's just
    a user note about action that places on the
    next line. If line doesn't starts with '-' or
    digit symbol, this line will be interpreted
    as the description of the last action.
    - validateActionString and
     calculateEfficiency functions
     moved down in the file
    - Action RegExp modified to find action type
    - Nature letter switch statement separated
    into specific function 'natureLetterToNature'
    - analyzeTime function now parses actions and
    than gets the efficiency by calculateEfficiency function
    - validateActionString function now has if
    statement to check if action type exists in
    global action types array

--- src/core/timeAnalyzer.ts
+++ src/core/timeAnalyzer.ts
@@ -2,40 +2,31 @@ interface IAction {
     name: string
     startTime?: Date
     endTime: Date
-    nature: 'positive' | 'neutral' | 'negative'
+    nature: TNature
+    type: TAction
+    description: string
 }
 
-function validateActionString(initialString: string, actionRegExp: RegExpMatchArray | null) {
-
-    if (!actionRegExp || !actionRegExp.groups) throw new Error(`Error! Action "${initialString}" has bad formatting!`)
-    if (!actionRegExp.groups.name) throw new Error(`Error! Action "${initialString}" has no name!`)
-    if (!actionRegExp.groups.endTimeHours || !actionRegExp.groups.endTimeMinutes) throw new Error(`Error! Action "${initialString}" has no end time!`)
-
-}
-
-function calculateEfficiency(actions: IAction[]): number | any {
-    const startDayTime = actions[0].startTime
-    const endDayTime = actions.slice(-1)[0].endTime
-
-    const wholeDayTime = endDayTime.valueOf() - startDayTime!.valueOf()
-
-    let positiveActionsTime = actions.reduce((acc, action) => action.nature === 'positive' ? acc + getActionDuration(action) : acc, 0)
-    if(positiveActionsTime === 0) return 0
-
-    return (positiveActionsTime / wholeDayTime * 100).toFixed(2)
-}
-
-function getActionDuration(action: IAction) {
-    return action.startTime ? action.endTime.valueOf() - action.startTime.valueOf() : 0
-}
+type TNature = 'positive' | 'neutral' | 'negative'
+type TAction = 'sport' | 'default'
+const actionTypes = ['sport', 'default']
 
 function parseActions(actionsString: string): IAction[] {
     const stringActions = actionsString.split('\n')
     const actions: IAction[] = []
+
     for (let stringAction of stringActions) {
         if (stringAction === '') continue
 
-        const actionRegExp = Array.from(stringAction.matchAll(/((?<startTimeHours>[012]?\d):(?<startTimeMinutes>[0-5]\d))?-(?<endTimeHours>[012]?\d):(?<endTimeMinutes>[0-5]\d)\)\s*(?<name>[^#]+)#?(?<nature>[PNnПНн])?/g))[0]
+        const previousAction = actions.length && actions.slice(-1)[0]
+
+        //Check if string is an action description
+        if (!/^[\d\-]/.test(stringAction)) {
+            previousAction && (previousAction.description += stringAction)
+            continue
+        }
+
+        const actionRegExp = Array.from(stringAction.matchAll(/((?<startTimeHours>[012]?\d):(?<startTimeMinutes>[0-5]\d))?-(?<endTimeHours>[012]?\d):(?<endTimeMinutes>[0-5]\d)\)\s*(?<name>[^#.]+)\s#?(?<nature>[PNnПНн])?(\.(?<type>\w+))?/g))[0]
 
         validateActionString(stringAction, actionRegExp)
 
@@ -43,6 +34,7 @@ function parseActions(actionsString: string): IAction[] {
         const name = actionRegExp!.groups!.name
         const startTime = new Date(0, 0, 0, +actionRegExp!.groups!.startTimeHours, +actionRegExp!.groups!.startTimeMinutes)
         const endTime = new Date(0, 0, 0, +actionRegExp!.groups!.endTimeHours, +actionRegExp!.groups!.endTimeMinutes)
+        const type = actionRegExp!.groups!.type as TAction
 
         //check time validity
         const timeValidity = checkTimeValidity(stringAction, startTime, endTime)
@@ -50,25 +42,9 @@ function parseActions(actionsString: string): IAction[] {
 
         //define nature of action
         const natureLetter = actionRegExp!.groups!.nature
-        let nature: 'positive' | 'neutral' | 'negative'
-        switch (natureLetter) {
-            case 'P':
-            case 'П':
-                nature = 'positive'
-                break
-            case 'N':
-            case 'Н':
-                nature = 'negative'
-                break
-            case 'n':
-            case 'н':
-                nature = 'neutral'
-                break
-            default:
-                nature = 'neutral'
-        }
+        const nature = natureLetterToNature(natureLetter)
 
-        const action: IAction = {name, startTime, endTime, nature}
+        const action: IAction = {name, startTime, endTime, nature, description: '', type: type || 'default'}
         actions.push(action)
     }
     return actions
@@ -81,21 +57,63 @@ function parseActions(actionsString: string): IAction[] {
         if (endTime <= startTime) return `Error! Action "${stringAction}" end time must be higher than start time!`
         return 'ok'
     }
+
+    function validateActionString(initialString: string, actionRegExp: RegExpMatchArray | null) {
+        if (!actionRegExp || !actionRegExp.groups) throw new Error(`Error! Action "${initialString}" has bad formatting!`)
+        if (!actionRegExp.groups.name) throw new Error(`Error! Action "${initialString}" has no name!`)
+        if (!actionRegExp.groups.endTimeHours || !actionRegExp.groups.endTimeMinutes) throw new Error(`Error! Action "${initialString}" has no end time!`)
+        if (actionRegExp.groups.type && !actionTypes.includes(actionRegExp.groups.type)) throw new Error(`Error! Action type "${actionRegExp.groups.type}" doesn't exist!`)
+    }
+
+    function natureLetterToNature(natureLetter: string): TNature {
+        switch (natureLetter) {
+            case 'P':
+            case 'П':
+                return 'positive'
+            case 'N':
+            case 'Н':
+                return 'negative'
+            case 'n':
+            case 'н':
+                return 'neutral'
+            default:
+                return 'neutral'
+        }
+    }
 }
 
-export function analyzeTime(actionsString: string): IAnalyzeResult | IAnalyzeResultError | void {
-    const actions = parseActions(actionsString)
-    //for test purposes
-    console.log(actions)
+function calculateEfficiency(actions: IAction[]): number | any {
+    const startDayTime = actions[0].startTime
+    const endDayTime = actions.slice(-1)[0].endTime
+
+    const wholeDayTime = endDayTime.valueOf() - startDayTime!.valueOf()
+
+    let positiveActionsTime = actions.reduce((acc, action) => action.nature === 'positive' ? acc + getActionDurationMs(action) : acc, 0)
+    if (positiveActionsTime === 0) return 0
+
+    return (positiveActionsTime / wholeDayTime * 100).toFixed(2)
+}
+
+function getActionDurationMs(action: IAction) {
+    return action.startTime ? action.endTime.valueOf() - action.startTime.valueOf() : 0
 }
 
 export interface IAnalyzeResult {
     positiveActionsPercent: number
     neutralActionsPercent: number
     negativeActionsPercent: number
+    efficiency: number
 }
 
 export interface IAnalyzeResultError {
-    error: boolean
     errorMessage: string
+}
+
+export function analyzeTime(actionsString: string): IAnalyzeResult | IAnalyzeResultError | any {
+    try {
+        const actions = parseActions(actionsString)
+        const efficiency = calculateEfficiency(actions)
+    } catch (e: any) {
+        return {errorMessage: e.message}
+    }
 }
