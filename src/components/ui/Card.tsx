c2df3ac1c0cc2e92c6d3a200f59ce5c04434d4e8
Buor <buor0@mail.ru>
2022-02-03 11:27:29 +0300

    style(EfficiencyBlock.tsx): appear animation added
    - Also code formatted

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
-import {Box, useStyleConfig} from '@chakra-ui/react';
+import {Box, BoxProps, useStyleConfig} from '@chakra-ui/react';
+import {motion} from 'framer-motion';
+
+const MotionBox = motion<BoxProps>(Box)
 
 export const Card: React.FC<any> = (props) => {
     const { variant, children, ...rest } = props
     const styles = useStyleConfig('Card', { variant })
-    return <Box __css={styles} {...rest} >{children}</Box>
+    return <MotionBox __css={styles} {...rest} >{children}</MotionBox>
 }
