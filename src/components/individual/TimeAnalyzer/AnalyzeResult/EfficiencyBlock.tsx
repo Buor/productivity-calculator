ac857e2e8e7f2ad32219c8cae24b27444c2b990d
Buor <buor0@mail.ru>
2022-02-03 09:27:49 +0300

    feat(EfficiencyBlock.tsx): component styled, logic implemented

--- src/components/individual/TimeAnalyzer/AnalyzeResult/EfficiencyBlock.tsx
+++ src/components/individual/TimeAnalyzer/AnalyzeResult/EfficiencyBlock.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
-import {IActionsPercentage} from '../../../../core/timeAnalyzer';
+import {IActionPercentage} from '../../../../core/timeAnalyzer';
 import {Box} from '@chakra-ui/react';
+import {Card} from '../../../ui/Card';
 
 interface IProps {
-    actionsPercentage: IActionsPercentage
+    actionsPercentage: IActionPercentage[]
 }
 
 export const EfficiencyBlock: React.FC<IProps> = ({actionsPercentage}) => {
+
     return (
-        <Box>
+        <Box d={'flex'} justifyContent={'space-evenly'}
+             flexWrap={'wrap'} mt={4}>
+
+            {actionsPercentage.map(percentage =>
+                <Card key={percentage.name}
+                      flex={['0 1 80%', '0 1 25%']}
+                      mt={[4, null, 0]}
+                      _hover={{
+                          cursor: 'pointer',
+                          bg: 'custom.mediumDark'
+                      }}
+                >
+                    <Box color={'white'} as={'h3'} fontSize={24} textAlign={'center'}
+                         w={['auto', '150px']}>{percentage.name}</Box>
+                    <Box color={percentage.color || 'white'} mt={4} fontSize={24}>{percentage.percentage + '%'}</Box>
+                </Card>)}
 
         </Box>
     )
