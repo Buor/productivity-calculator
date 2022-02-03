1307976d4b54508f2b7cb2ca577dc1dce5a6a159
Buor <buor0@mail.ru>
2022-02-03 11:36:16 +0300

    style(Navbar.tsx): selected navbar element highlights now

--- src/components/individual/Navbar.tsx
+++ src/components/individual/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Box, Button} from '@chakra-ui/react'
 import {routes} from '../../routes/routes'
 import {useNavigate} from 'react-router-dom'
@@ -10,6 +10,7 @@ interface IProps {
 export const Navbar: React.FC<IProps> = () => {
 
     const navigate = useNavigate()
+    const [currentRoute, setCurrentRoute] = useState(window.location.pathname)
 
     return (
         <Box
@@ -24,14 +25,18 @@ export const Navbar: React.FC<IProps> = () => {
                     key={route.link}
                     flex={'1 1 200px'}
                     border={'0'}
-                    backgroundColor={'custom.dark'}
+                    backgroundColor={currentRoute === route.link ? 'custom.medium' : 'custom.dark'}
                     fontWeight={'700'}
                     fontSize={17}
+                    borderRadius={0}
                     _hover={{
                         backgroundColor: 'custom.medium'
                     }}
                     _focus={{border: '0'}}
-                    onClick={() => navigate(route.link)}
+                    onClick={() => {
+                        setCurrentRoute(route.link)
+                        navigate(route.link)
+                    }}
                 >
                     {route.name}
                 </Button>
