c1d3944baf65c07bc4c69c5be537d87d139c418b
Buor <buor0@mail.ru>
2022-02-12 19:16:01 +0300

    feat(ProductivityBlock.tsx): component has been implemented

--- src/components/individual/TimeAnalyzer/AnalyzeResult/ProductivityBlock.tsx
+++ src/components/individual/TimeAnalyzer/AnalyzeResult/ProductivityBlock.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
-import {Box} from '@chakra-ui/react';
+import {Text} from '@chakra-ui/react';
+import {Card} from "../../../ui/Card";
+import {IProductivity} from "../../../../core/timeAnalyzer";
 
 interface IProps {
-    productivity: number
+    productivity: IProductivity
 }
 
 export const ProductivityBlock: React.FC<IProps> = ({productivity}) => {
     return (
-        <Box>
-
-        </Box>
+        <Card maxWidth={500}
+              mx={'auto'}
+              mt={8}
+              boxShadow={'2px 2px 10px 2px rgba(0.5,0.5,0.5,0.5)'}
+              opacity={0}
+              animate={{opacity: 100}}
+              transition={{delay: .1, duration: 7}}
+        >
+            <Text textAlign={'center'}>Your productivity value today:</Text>
+            <Text fontSize={24} color={productivity.color}>{productivity.value}</Text>
+            <Text color={productivity.color}>{productivity.comment}</Text>
+        </Card>
     )
 }
