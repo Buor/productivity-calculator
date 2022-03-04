5beb1ba3185791c08a1674cb190f3871239aac6a
Buor <buor0@mail.ru>
2022-03-04 10:18:27 +0300

    feat(client): new empty calendar component has been created and bound to routes

--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import {TimeAnalyzer} from "../components/individual/TimeAnalyzer/TimeAnalyzer"
 import {Main} from '../components/individual/Main';
+import {Calendar} from "../components/individual/Calendar/Calendar";
 
 export interface IRoute {
     link: string
@@ -18,5 +19,10 @@ export const routes: IRoute[] = [
         link: '/',
         name: 'Main',
         component: <Main/>
+    },
+    {
+        link: '/calendar',
+        name: 'Calendar',
+        component: <Calendar/>
     }
 ]
