a32cd97c89833c25b5f250cc36e281b3db84ca64
Buor <buor0@mail.ru>
2022-04-16 12:15:06 +0300

    feat(server): generateDatesForFetch func implemented and tested

--- server/src/utils/timeHelpers/timeHelpers.spec.ts
+++ server/src/utils/timeHelpers/timeHelpers.spec.ts
@@ -1,4 +1,4 @@
-import {formatDate, getDateHM, hmToDate} from "./timeHelpers";
+import {formatDate, generateDatesForFetch, getDateHM, hmToDate} from "./timeHelpers";
 
 describe('date format', () => {
     test('test date 1', () => {
@@ -27,5 +27,25 @@ describe('hm to date', () => {
     })
     test('hm to correct date transform 2', () => {
         expect(hmToDate('00:00')).toEqual(new Date(0,0,0,0,0))
+    })
+})
+
+describe('generate dates for fetch', () => {
+    test('correct data for january 2022', () => {
+        const result = generateDatesForFetch(new Date(2022, 0))
+        expect(result).toEqual(['2021-12-27', '2021-12-28', '2021-12-29',
+            '2021-12-30', '2021-12-31', '2022-01-01',
+            '2022-01-02', '2022-01-03', '2022-01-04',
+            '2022-01-05', '2022-01-06', '2022-01-07',
+            '2022-01-08', '2022-01-09', '2022-01-10',
+            '2022-01-11', '2022-01-12', '2022-01-13',
+            '2022-01-14', '2022-01-15', '2022-01-16',
+            '2022-01-17', '2022-01-18', '2022-01-19',
+            '2022-01-20', '2022-01-21', '2022-01-22',
+            '2022-01-23', '2022-01-24', '2022-01-25',
+            '2022-01-26', '2022-01-27', '2022-01-28',
+            '2022-01-29', '2022-01-30', '2022-01-31',
+            '2022-02-01', '2022-02-02', '2022-02-03',
+            '2022-02-04', '2022-02-05', '2022-02-06'])
     })
 })
--- server/src/utils/timeHelpers/timeHelpers.ts
+++ server/src/utils/timeHelpers/timeHelpers.ts
@@ -3,15 +3,31 @@
  *
  */
 export function formatDate(date: Date, type: 'dot' | 'dash' = 'dot') {
-    if(type === 'dot') return `${getStrDate(date)}.${getStrMonth(date)}.${date.getFullYear()}`
+    if (type === 'dot') return `${getStrDate(date)}.${getStrMonth(date)}.${date.getFullYear()}`
     return `${date.getFullYear()}-${getStrMonth(date)}-${getStrDate(date)}`
 }
 
-export const getDateHM = (date: Date) =>  `${('0' + date.getHours()).slice(-2)}:${('0' + date.getMinutes()).slice(-2)}`
-export const hmToDate = (hm: string) => new Date(0,0,0, +hm.slice(0,2), +hm.slice(-2))
+export const getDateHM = (date: Date) => `${('0' + date.getHours()).slice(-2)}:${('0' + date.getMinutes()).slice(-2)}`
+export const hmToDate = (hm: string) => new Date(0, 0, 0, +hm.slice(0, 2), +hm.slice(-2))
 const getStrMonth = (date: Date) => ('0' + (date.getMonth() + 1)).slice(-2)
 const getStrDate = (date: Date) => ('0' + date.getDate()).slice(-2)
 
-// export function formMonthData(monthTimeStamp) {
-//
-// }
+export function generateDatesForFetch(month: Date) {
+    const dates: Date[] = []
+    const dateToIter = new Date(month.getFullYear(), month.getMonth(), 1)
+    const monthNumber = month.getMonth()
+
+    let decr: number
+
+    if(dateToIter.getDay() === 0) decr = 6
+    else decr = dateToIter.getDay() - 1
+
+    dateToIter.setDate(dateToIter.getDate() - decr)
+
+    //Fill with month dates
+    for (; !(dateToIter.getMonth() === monthNumber + 1 && dateToIter.getDay() === 1); dateToIter.setDate(dateToIter.getDate() + 1)) {
+        dates.push(new Date(+dateToIter))
+    }
+
+    return dates
+}
